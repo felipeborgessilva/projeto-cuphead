e29e3cc6d33a63e557208b875c760470dac857ed
felipeborgessilva <70147361+felipeborgessilva@users.noreply.github.com>
2021-10-08 00:12:02 -0300

    Criado feature de dash do personagem

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,12 +12,22 @@ public class PlayerController : MonoBehaviour
     public Transform groundCheck;
     private bool estaNoChao;
 
+    float doubleTapTime;
+    KeyCode lastKeyCode;
+    public float dashSpeed;
+    private float dashCount;
+    public float startDashCount;
+    private int side;
+    private bool dashAir;
+
     // Start is called before the first frame update
     void Start()
     {
         // coleta o componente de regidbody 
         meuRigidBody = GetComponent<Rigidbody2D>();
         meuAnimator = GetComponent<Animator>();
+
+        dashCount = startDashCount;
     }
 
     // Update is called once per frame
@@ -48,6 +58,35 @@ public class PlayerController : MonoBehaviour
         meuRigidBody.velocity = new Vector2(horizontal * velocidade, velocidadeY);
         meuAnimator.SetInteger("h", (int) horizontal);
         meuAnimator.SetBool("isGrounded", estaNoChao);
+        
+        // dash
+        if (side == 0)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                side = 1;
+            }
+        }
+        else
+        {
+            if (dashCount <= 0)
+            {
+                side = 0;
+                dashCount = startDashCount;
+                meuRigidBody.velocity = Vector2.zero;
+                dashAir = false;
+            } else {
+                dashCount -= Time.deltaTime;
+                if (olhandoParaEsquerda)
+                {
+                    meuRigidBody.velocity = Vector2.left * dashSpeed;
+                } else if(!olhandoParaEsquerda)
+                {
+                    meuRigidBody.velocity = Vector2.right * dashSpeed;
+                }
+            }
+        }
+
     }
 
     void FixedUpdate() 
@@ -65,4 +104,5 @@ public class PlayerController : MonoBehaviour
         float x = transform.localScale.x * -1;
         transform.localScale = new Vector3(x, transform.localScale.y, transform.localScale.z);
     }
+
 }
