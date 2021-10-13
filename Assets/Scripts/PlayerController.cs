a32ee3bec904f77a998303abd1128360a3ac4608
felipeborgessilva <70147361+felipeborgessilva@users.noreply.github.com>
2021-10-13 03:37:53 -0300

    Ajustando jogabilidade do player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,111 +4,88 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D meuRigidBody;
-    private Animator meuAnimator;
-    public float velocidade;
-    public float forcaPulo;
-    public bool olhandoParaEsquerda;
-    public Transform groundCheck;
-    private bool estaNoChao;
-
-    float doubleTapTime;
-    KeyCode lastKeyCode;
-    public float dashSpeed;
-    private float dashCount;
-    public float startDashCount;
-    private int side;
-    private float dashCooldown;
-    private bool estaNoDash;
+    private Rigidbody2D rig;
+    private Animator anim;
+    public float Speed;
+    public float JumpForce;
+    public bool IsJumping;
+    public float DashForce;
 
     // Start is called before the first frame update
     void Start()
     {
-        // coleta o componente de regidbody 
-        meuRigidBody = GetComponent<Rigidbody2D>();
-        meuAnimator = GetComponent<Animator>();
-
-        dashCount = startDashCount;
+        // coleta o componente Rigidbody2D
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // coleta o input do movimento horizontal
-        float horizontal = Input.GetAxisRaw("Horizontal");
-
-        if (horizontal > 0 && olhandoParaEsquerda)
-        {
-            Flip();
-        } else if (horizontal < 0 && !olhandoParaEsquerda)
-        {
-            Flip();
-        }
-
-        float velocidadeY = meuRigidBody.velocity.y;
-
-        if (Input.GetButtonDown("Fire3") && estaNoChao)
-        {
-            meuRigidBody.AddForce(new Vector2(
-                    0.0f, 
-                    forcaPulo
-                )
-            );
-        }
-
-        meuRigidBody.velocity = new Vector2(horizontal * velocidade, velocidadeY);
-        meuAnimator.SetInteger("h", (int) horizontal);
-        meuAnimator.SetBool("isGrounded", estaNoChao);
-        
-        // dash
-        dashCooldown -= Time.deltaTime;
-        if (side == 0)
-        {
-            if (Input.GetButtonDown("Fire1") && dashCooldown <= 0)
-            {
-                side = 1;
-                dashCount = startDashCount;
-                estaNoDash = true;
-            } else {
-                estaNoDash = false;
-            }
-        }
-        else
-        {
-            if (dashCount <= 0)
-            {
-                dashCount = startDashCount;
-                side = 0;
-            } else {
-                dashCount -= Time.deltaTime;
-                if (olhandoParaEsquerda)
-                {
-                    meuRigidBody.velocity = Vector2.left * dashSpeed;
-                } else
-                {
-                    meuRigidBody.velocity = Vector2.right * dashSpeed;
-                }
-                dashCooldown = 0.8f;
-            }
-        }
-        meuAnimator.SetBool("isDashing", estaNoDash);
-
+        // chama a função que movementa do personagem
+        Move();
+        // chama a função que ativa o pulo do personagem
+        Jump();
     }
 
-    void FixedUpdate() 
+    // movimenta o player
+    void Move()
     {
-        // verifica a colisão
-        estaNoChao = Physics2D.OverlapCircle(groundCheck.position, 0.02f);
+        // aplica coleta movimento com base ativação do pesonagem com o teclado
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        // aplica a movimentação no personagem com os valores inputados pelo teclado do jogador 
+        transform.position += movement * Time.deltaTime * Speed;
+        // se o personagem for para a direita
+        if(Input.GetAxis("Horizontal") > 0) 
+        {
+            // altera para true a variável da animação
+            anim.SetBool("walk", true);
+            // ajusta o lado da sprite
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        // se o personagem for para a esquerda
+        } else if(Input.GetAxis("Horizontal") < 0) {
+            // altera para true a variável da animação
+            anim.SetBool("walk", true);
+            // ajusta o lado da sprite
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        // senão
+        } else {
+            // altera para false a variável da animação
+            anim.SetBool("walk", false);
+        }
     }
 
-    // função para flipar o personagem
-    void Flip()
+    // pulo do player
+    void Jump()
     {
-        // inverte a variável
-        olhandoParaEsquerda = !olhandoParaEsquerda;
-        // inverte o sinal do scale x
-        float x = transform.localScale.x * -1;
-        transform.localScale = new Vector3(x, transform.localScale.y, transform.localScale.z);
+        // verifica que o Jump foi apertado e se o personagem não está pulando
+        if (Input.GetButtonDown("Jump") && !IsJumping)
+        {
+            // aplica a força do pulo
+            rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+        }
     }
 
+    // chamada quando alguma colisão de entrada é detectada
+    private void OnCollisionEnter2D(Collision2D collision) {
+        // verifica se a layer que colidiu é a 8
+        if (collision.gameObject.layer == 8)
+        {
+            // altera para false a variável que controla se o player está pulando
+            IsJumping = false;
+            // altera para false a variável da animação
+            anim.SetBool("jump", false);
+        }
+    }
+    // chamada quando alguma colisão de saída é detectada
+    private void OnCollisionExit2D(Collision2D collision) {
+        // verifica se a layer que colidiu é a 8
+        if (collision.gameObject.layer == 8)
+        {
+            // altera para true a variável que controla se o player está pulando
+            IsJumping = true;
+            // altera para true a variável da animação
+            anim.SetBool("jump", true);
+        }
+    }
 }
